cf318f4f767e8ccf2dbc9b4d144bf210f875b77b
rafael-m-silva <rafael_mauricio@hotmail.com>
2023-06-02 12:57:49 -0300

    feat: animação menu navbar

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 const btn = document.querySelector(".section-squad .section-squad-pedding .img img")
 const btnForm = document.querySelector("#btn-form")
+const menu = document.querySelector(".menu")
 
+// Animação de cores na seção SQUAD
 const handleCores = () => {
   const sectionColor = document.querySelector(".section-squad-pedding")
 
   sectionColor.classList.toggle("gray-off")
 }
 
-
+// Reset input após o envio
 const handleSubmit = (e) => {
   e.preventDefault()
 
@@ -26,5 +28,17 @@ const handleSubmit = (e) => {
     textArea.value = ""
 }
 
+// Animação do menu navbar
+const animationMenu = () => {
+  const firstDivMenu = document.querySelector(".menu div:nth-child(1)")
+  const secondDivMenu = document.querySelector(".menu div:nth-child(2)")
+  const thirdDivMenu = document.querySelector(".menu div:nth-child(3)")
+
+  firstDivMenu.classList.toggle("menu-first")
+  secondDivMenu.classList.toggle("menu-second")
+  thirdDivMenu.classList.toggle("menu-third")
+}
+
+menu.addEventListener("click", animationMenu)
 btn.addEventListener("click", handleCores)
 btnForm.addEventListener("click", handleSubmit)
